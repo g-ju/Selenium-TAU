c35ab0191c92e0ca7386b7c7ec62bc9d13fc73bf
garth <52281882+g-ju@users.noreply.github.com>
2023-08-07 16:03:07 +0100

    Login test
    -Create some classes for relevant pages
    -Actually use a test framework

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -2,27 +2,30 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
 public class BaseTests
 {
     private WebDriver driver;
 
+    protected HomePage homePage;
+
+    @BeforeClass
     public void setUp()
     {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        driver.manage().window().maximize();
-
-        System.out.println(driver.getTitle());
-        driver.quit();
+        homePage = new HomePage(driver);
     }
 
-    public static void main(String args[])
+    @AfterClass
+    public void tearDown()
     {
-        BaseTests test = new BaseTests();
-        test.setUp();
+        driver.quit();
     }
 }
 
